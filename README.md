07d3d14ca297d83f0c40b8698de98e383c567c4e
sittiphansittisak-jobinterview <sittiphansittisak.jobinterview@gmail.com>
2023-05-07 20:50:37 +0700

    project initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# cu_se_form
---
+++ client_flutter/android/app/src/main/kotlin/sittiphansittisak/job_interview/cu_se_form/client_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package sittiphansittisak.job_interview.cu_se_form.client_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
