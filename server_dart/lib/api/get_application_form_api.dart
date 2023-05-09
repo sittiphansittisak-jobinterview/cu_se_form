47e510d62e9db36721c220ed8c7be864df6dc26b
sittiphansittisak-jobinterview <sittiphansittisak.jobinterview@gmail.com>
2023-05-09 13:37:44 +0700

    server_dart: add GetApplicationFormApi

---
+++ server_dart/lib/api/get_application_form_api.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:server_dart/controller/get_application_form_controller.dart';
+import 'package:server_dart/private/utility/generate_respone.dart';
+import 'package:share_flutter/object/key/application_form_key.dart';
+import 'package:share_flutter/private/utility/map_filter.dart';
+import 'package:share_flutter/utility/my_alert_message.dart';
+import 'package:shelf/shelf.dart';
+
+Future<Response> getApplicationFormApi(Request request) async {
+  final GetApplicationFormController controller = GetApplicationFormController(request: request);
+  try {
+    if (!await controller.receiveRequest()) return generateResponse(message: controller.messageResponse);
+    if (!await controller.validateRequest()) return generateResponse(message: controller.messageResponse);
+    if (!await controller.findOtp()) return generateResponse(message: controller.messageResponse);
+    if (!await controller.validateOtp()) return generateResponse(message: controller.messageResponse);
+    if (!await controller.findUser()) return generateResponse(message: controller.messageResponse);
+    if (!await controller.findApplicationForm()) return generateResponse(message: controller.messageResponse);
+    controller.applicationFormResponse
+      ..toObject()
+      ..toMap(); //reformat
+    controller.applicationFormResponse.map = mapFilter(controller.applicationFormResponse.map, removeKey: [ApplicationFormKey.id, ApplicationFormKey.userId]);
+    return generateResponse(httpStatus: HttpStatus.ok, isSuccess: true, data: {'applicationForm': controller.applicationFormResponse.map});
+  } catch (e) {
+    return generateResponse(httpStatus: HttpStatus.internalServerError, message: '$e\n${MyAlertMessage.reportIssue}');
+  }
+}
